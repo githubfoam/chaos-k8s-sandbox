d1d5357ae0a2ca959635776f7042126dfb27570c
Your Name <you@example.com>
2022-06-08 16:15:03 +0300

    v3.9.0

--- app/provision-helm.sh
+++ app/provision-helm.sh
@@ -5,20 +5,29 @@ set -o nounset
 set -o xtrace
 # set -eox pipefail #safety for script
 
-
+https://helm.sh/docs/intro/install/
 echo fetch and install helm...
-HELM_ARCHIVE=helm-v2.12.1-linux-amd64.tar.gz
+# HELM_ARCHIVE=helm-v2.12.1-linux-amd64.tar.gz
+# https://github.com/helm/helm/releases
+HELM_ARCHIVE=helm-v3.9.0-linux-amd64.tar.gz
 HELM_DIR=linux-amd64
 HELM_BIN=$HELM_DIR/helm
-curl -LsO https://storage.googleapis.com/kubernetes-helm/$HELM_ARCHIVE && tar -zxvf $HELM_ARCHIVE && chmod +x $HELM_BIN && cp $HELM_BIN /usr/local/bin
+
+# curl -LsO https://storage.googleapis.com/kubernetes-helm/$HELM_ARCHIVE && tar -zxvf $HELM_ARCHIVE \
+# && chmod +x $HELM_BIN && cp $HELM_BIN /usr/local/bin
+
+
+curl -LsO https://get.helm.sh/$HELM_ARCHIVE && tar -zxvf $HELM_ARCHIVE \
+&& chmod +x $HELM_BIN && cp $HELM_BIN /usr/local/bin
+
 rm $HELM_ARCHIVE
 rm -rf $HELM_DIR
 
 helm version
 
-echo setup tiller account...
-kubectl -n kube-system create sa tiller && kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+# echo setup tiller account...
+# kubectl -n kube-system create sa tiller && kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 
-echo initialize tiller...
-helm init --wait --skip-refresh --upgrade --service-account tiller
-echo tiller initialized
+# echo initialize tiller...
+# helm init --wait --skip-refresh --upgrade --service-account tiller
+# echo tiller initialized
